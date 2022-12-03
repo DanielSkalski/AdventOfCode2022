029fd6665152ca04ee91d90098ec68dd41d03be0
Daniel <daniel.skalski@gmail.com>
2022-12-03 08:56:36 +0100

    day 1 & day 2 solutions

---
+++ day1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+var filepath = "./data.txt";
+
+var currentElf = 0;
+var currentMax1 = 0;
+var currentMax2 = 0;
+var currentMax3 = 0;
+
+foreach(var line in await File.ReadAllLinesAsync(filepath))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (currentElf > currentMax1)
+        {
+            currentMax3 = currentMax2;
+            currentMax2 = currentMax1;
+            currentMax1 = currentElf;
+        }
+        else if (currentElf > currentMax2)
+        {
+            currentMax3 = currentMax2;
+            currentMax2 = currentElf;
+        }
+        else if (currentElf > currentMax3)
+        {
+            currentMax3 = currentElf;
+        }
+        currentElf = 0;
+    }
+    else
+    {
+        var calories = Convert.ToInt32(line);
+        currentElf += calories;
+    }
+}
+
+Console.WriteLine(currentMax1 + currentMax2 + currentMax3);
---
+++ day2/Program.cs
@@ -0,0 +1,48 @@
+﻿var datafile = "./data.txt";
+
+int bonusScore(char opponent, char roundResult) 
+{
+    return (opponent, roundResult) switch
+    {
+        ('A', 'X') => 3,
+        ('A', 'Y') => 1,
+        ('A', 'Z') => 2,
+
+        ('B', 'X') => 1,
+        ('B', 'Y') => 2,
+        ('B', 'Z') => 3,
+
+        ('C', 'X') => 2,
+        ('C', 'Y') => 3,
+        ('C', 'Z') => 1,
+
+        _ => 0
+    };
+};
+
+int winLoseScore(char roundResult) 
+{
+    return roundResult switch
+    {
+        'X' => 0,
+        'Y' => 3,
+        'Z' => 6,
+        _ => 0
+    };
+}
+
+var totalScore = 0;
+
+foreach(var line in await File.ReadAllLinesAsync(datafile))
+{
+    var opponentChoice = line[0];
+    var roundResult = line[2];
+
+    var roundScore = 
+        bonusScore(opponentChoice, roundResult) +
+        winLoseScore(roundResult);
+
+    totalScore += roundScore;
+}
+
+Console.WriteLine(totalScore);
