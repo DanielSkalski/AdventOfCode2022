269910d2cfaaa8b84f03f7cb8ec1780176788ed3
Daniel <daniel.skalski@gmail.com>
2022-12-04 10:32:43 +0100

    day 4 part 1 solution

---
+++ day4/Program.cs
@@ -0,0 +1,28 @@
+﻿var datafile = "data.txt";
+
+var result = 0;
+
+foreach (var line in File.ReadAllLines(datafile))
+{
+    var elves = line.Split(",");
+    var firstElfRange = elves[0].Split("-");
+    var firstElfStart = Convert.ToInt32(firstElfRange[0]);
+    var firstElfEnd = Convert.ToInt32(firstElfRange[1]);
+
+    var secondElfRange = elves[1].Split("-");
+    var secondElfStart = Convert.ToInt32(secondElfRange[0]);
+    var secondElfEnd = Convert.ToInt32(secondElfRange[1]);
+
+    if (firstElfStart >= secondElfStart &&
+        firstElfEnd <= secondElfEnd)
+    {
+        result++;
+    }
+    else if (secondElfStart >= firstElfStart &&
+        secondElfEnd <= firstElfEnd)
+    {
+        result++;
+    }
+}
+
+Console.WriteLine(result);
