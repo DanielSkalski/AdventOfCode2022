2172485b799106d755a14ec634708da0b09b84aa
Daniel <daniel.skalski@gmail.com>
2022-12-10 11:14:43 +0100

    day 10 part 1 solution

---
+++ day10/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+var datafile = "data.txt";
+
+int[] cycles = { 20, 60, 100, 140, 180, 220 };
+
+var instructions = File.ReadAllLines(datafile);
+
+var register = 1;
+int cycle = 1;
+var signalStrengths = new List<int>();
+
+foreach(var instruction in instructions)
+{
+    if (cycles.Contains(cycle))
+    {
+        signalStrengths.Add(cycle * register);
+    }
+
+    var command = instruction.Split(" ");
+    if (command[0] == "addx")
+    {
+        var value = Convert.ToInt32(command[1]);
+        cycle++;
+        if (cycles.Contains(cycle))
+        {
+            signalStrengths.Add(cycle * register);
+        }
+        register += value;
+    }
+
+    cycle++;
+}
+
+var result = signalStrengths.Sum();
+
+Console.WriteLine(result);
